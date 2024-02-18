efac71b0e3c715e83e09d29a1fc22c33804dcf17
Kevin Zheng <kezheng@ucsd.edu>
2024-02-18 00:21:29 -0800

    testing for US7

--- app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
@@ -17,6 +18,8 @@ import edu.ucsd.cse110.successorator.databinding.FragmentMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding view;
+
+    ImageButton imageButton2;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +32,17 @@ public class MainActivity extends AppCompatActivity {
         SimpleDateFormat date = new SimpleDateFormat("EEEE MM/dd", Locale.getDefault());
         String currentDate = date.format(new Date());
         v.dateText.setText(currentDate);
+        imageButton2 = (ImageButton) findViewById(R.id.imageButton2);
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            Calendar c = Calendar.getInstance();
+            @Override
+            public void onClick(View view) {
+                c.add(Calendar.DATE, 1);
+                String currentDate = date.format(c.getTime());
+                v.dateText.setText(currentDate);
+                setContentView(v.getRoot());
+            }
+        });
 
         //To test the empty goal text
         v.emptyGoals.setText(R.string.emptyGoalsText);
